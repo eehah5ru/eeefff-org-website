932e81d669e1114f4265078a100f9f6ac49a16b4
Nicola Spesivtsev <nicola.spesivcev@gmail.com>
2016-01-08 00:57:42 +0300

    added basic Gruntfile

---
+++ Gruntfile.js
@@ -0,0 +1,79 @@
+module.exports = function(grunt) {
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+
+    sass: {
+      options: {
+        includePaths: ['bower_components/foundation-sites/',
+                       'bower_components/foundation-sites/scss',
+                      ]
+      },
+      dist: {
+        options: {
+          outputStyle: 'nested',
+          sourceMap: 'inline',
+          lineNumbers: true
+        },
+        files: {
+          'static/css/app.css': 'scss/app.scss'
+        }
+      }
+    },
+
+    bower_concat: {
+      all:
+      {
+        exclude: [
+          'jquery',
+          'modernizr',
+          'what-input'
+        ],
+        dest: 'static/js/bower.js',
+        dependencies: {
+
+          // 'foundation-sites': ['jquery'
+          //          ]
+
+        }
+      }
+    },
+
+    bowercopy: {
+      options: {
+        srcPrefix: 'bower_components'
+      },
+      scripts: {
+        options: {
+          destPrefix: 'static/js'
+        },
+        files: {
+          'vendor/jquery.js': 'jquery/dist/jquery.js',
+          'vendor/modernizr.js': 'modernizr/modernizr.js',
+          'vendor/what-input.js': 'what-input/what-input.js'
+        }
+      }
+    },
+
+    watch: {
+      grunt: {
+        options: {
+          reload: true
+        },
+        files: ['Gruntfile.js']
+      },
+
+      sass: {
+        files: 'scss/**/*.scss',
+        tasks: ['sass']
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-sass');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-bower-concat');
+  grunt.loadNpmTasks('grunt-bowercopy');
+
+  grunt.registerTask('build', ['sass', 'bowercopy', 'bower_concat']);
+  grunt.registerTask('default', ['build','watch']);
+};
