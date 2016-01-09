48e9c87c7235903e5308d7db2cb0aa7a79e26761
Nicola Spesivtsev <nicola.spesivcev@gmail.com>
2016-01-09 03:40:16 +0300

    added minimal site skeleton

---
+++ templates/header.lisp
@@ -0,0 +1,13 @@
+$(xml
+  (:head
+   (:meta :charset "UTF-8")
+   (:meta :name "viewport" :content "width=device-width, initial-scale=.75")
+   (:title (str (when (bound? title)
+      (echo title " | ")))
+     (str site-name))
+   (:link :href "/static/css/app.css" :rel "stylesheet" :type "text/css" )
+   (:script :src "/static/js/vendor/jquery.js" :type "text/javascript")
+   (:script :src "/static/js/vendor/modernizr.js" :type "text/javascript")
+   (:script :src "/static/js/vendor/what-input.js" :type "text/javascript")
+   (:script :src "/static/js/bower.js" :type "text/javascript")
+   (:script :src "/static/js/app.js" :type "text/javascript")))
---
+++ templates/main.lisp
@@ -0,0 +1,11 @@
+$(xml
+  (:html
+   (str (include "header.lisp"))
+
+   (:body :data-sticky-container t
+    ;; (str (include "navigation.lisp"))
+
+    (str (include "teaser.lisp"))
+
+    (:script :type "text/javascript"
+       "jQuery(document).foundation();"))))
